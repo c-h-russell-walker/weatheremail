87b58d60646fb50b9a2784af03d56693ed5d2e22
Chris Russell-Walker <c.russell.walker@gmail.com>
2017-04-23 19:59:15 -0400

    Using dj database url alongside heroku deploy

--- weatheremail/weatheremail/settings.py
+++ weatheremail/weatheremail/settings.py
@@ -86,15 +86,10 @@ TEMPLATES = [
 WSGI_APPLICATION = 'weatheremail.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/1.10/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+# Update database configuration with $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
 
 
 # Password validation
